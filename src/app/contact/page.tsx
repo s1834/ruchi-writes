f850185c8d367273eca5a6b899a7562a81ac6f4b
s1834 <s18324@protonmail.com>
2024-10-27 11:45:20 +0530

    Added all base components

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Form from "@/components/form";
+import Newsletter from "@/components/newsletter";
+
+export default function Contact() {
+  return (
+    <div className="relative min-h-screen bg-white dark:bg-neutral-950">
+      <Form />
+      <Newsletter />
+    </div>
+  );
+}
