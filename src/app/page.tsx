b51e2e71eaaac8eabaf44ea5252a536af0b4ff2d
khushi-upadhyay <khushiupadhyay2004@gmail.com>
2024-12-06 18:32:58 +0530

    added about page and resolved version errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,13 @@ import Nav from "@/app/navbar/page";
 import Read from "@/app/read/page";
 import Contact from "@/app/contact/page";
 import Blogs from "@/app/blogs/page";
+import About from "./about/page";
 
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-white dark:bg-neutral-950">
       <Nav />
+      <About/>
       <Blogs />
       <Read />
       <Contact />
